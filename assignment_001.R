c2dd2c362e6e052eba2eb830204957f18be76990
machadob <burton.machado@gmail.com>
2014-09-01 16:21:12 -0400

    Assignment 1 Submission

---
+++ assignment_001.R
@@ -0,0 +1,4 @@
+print('The version of R is 3.1.1')
+print('The version of R Studio is 0.98.1028')
+print('The version of PostgreSQL is 9.3')
+print('The size of the observations in data set sales is 10')
